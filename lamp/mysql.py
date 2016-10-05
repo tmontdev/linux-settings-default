ddeb8d85e57fd7e168f5d2589e4dd9d1aa14e5bc
Marcos Felipe <omarkdev@gmail.com>
2016-10-05 20:40:12 -0300

    Separate installation files

---
+++ lamp/mysql.py
@@ -0,0 +1,12 @@
+import os
+
+# Install MySQL
+os.system("sudo pacman -Syu mariadb mariadb-clients libmariadbclient")
+
+# Install MariaDB data directory
+os.system("sudo mysql_install_db --user=mysql --basedir=/usr --datadir=/var/lib/mysql")
+
+# Start MySQL and set it to run at boot
+os.system("sudo systemctl start mysqld.service; sudo systemctl enable mysqld.service")
+
+os.system("mysql_secure_installation")
