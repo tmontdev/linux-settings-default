077525ba22e56ce7a4479514990185ed23662fd7
Marcos Felipe <omarkdev@gmail.com>
2016-10-05 22:14:31 -0300

    Add install composer in php-additionals

---
+++ php-additionals/composer.py
@@ -0,0 +1,14 @@
+import os
+
+# Install Composer
+os.system("curl -sS https://getcomposer.org/installer | php")
+
+os.system("sudo mv composer.phar /usr/local/bin/composer")
+
+# composr global require in terminal
+home = input('Your username linux (example: omarkdev): ')
+
+composer_path = '\nPATH=~/.config/composer/vendor/bin:$PATH'
+
+with open("/home/"+home+"/.zshrc", "a") as myfile:
+    myfile.write(composerpath)
