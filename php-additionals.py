21057e5ac8fa36b276e1596ac41803a83e16e0c1
Marcos Felipe <omarkdev@gmail.com>
2016-10-05 22:24:52 -0300

    Add install wp-cli in php-additionals

--- php-additionals.py
+++ php-additionals.py
@@ -8,3 +8,6 @@ os.system("sudo python php-additionals/laravel.py")
 
 # Install Lumen
 os.system("sudo python php-additionals/lumen.py")
+
+# Install wp-cli
+os.system("sudo python php-additionals/wp-cli.py")
